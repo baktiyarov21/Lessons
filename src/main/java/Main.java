4b19e78f192bf5f05f1c3c84218d0abe5ab81cfb
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-02-26 09:23:51 +0600

    Stream api in main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import stream_api.Director;
+import stream_api.Predicate;
+import stream_api.Worker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static int plus() {
-        return 100 + 200;
+    public static void main(String[] args) {
+       /* Director director = new Director();
+        Worker worker = i -> "Success " + i;
+        String string = director.doWork(worker,6);
+        System.out.println(string);*/
+
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            integers.add((int) (Math.random() * 1000));
+        }
+        List<Integer> filtered = filter(integers, a -> a % 2 == 0);
+        for (int numbers :
+                filtered) {
+            System.out.println(numbers);
+        }
+    }
+
+    public static List<Integer> filter(List<Integer> list, Predicate predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (predicate.predicate(i)) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
